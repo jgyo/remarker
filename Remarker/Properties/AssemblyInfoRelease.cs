f9b249f4fe4f8ca9cc8463171e347fecd8d3c5f7
gloder <gil.yoder@outlook.com>
2014-09-17 15:41:04 -0500

    Another vsix update to fix an install problem.

--- Remarker/Properties/AssemblyInfoRelease.cs
+++ Remarker/Properties/AssemblyInfoRelease.cs
@@ -30,7 +30,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.2.5374.35")]
-[assembly: AssemblyFileVersion("1.2.14260.1901")]
-[assembly: AssemblyInformationalVersion("2014.9.17.1901")]
+[assembly: AssemblyVersion("1.2.5374.36")]
+[assembly: AssemblyFileVersion("1.2.14260.1917")]
+[assembly: AssemblyInformationalVersion("2014.9.17.1917")]
 #endif
